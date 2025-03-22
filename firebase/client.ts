54b563a116fe5f28720d865e1523b49b9590edad
Neobyte-Design <neobytedesign@gmail.com>
2025-03-22 17:10:05 +0630

    firebase config update

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -20,7 +14,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
+
+// Enable `ignoreUndefinedProperties`
+export const db = initializeFirestore(app, {
+  ignoreUndefinedProperties: true,
+});
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
